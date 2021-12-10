10c0d3c47c6c6853294aeeed4e3f069adb4a094a
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-12-10 20:13:57 +0100

    Update tests.php

--- tests.php
+++ tests.php
@@ -1,6 +1,9 @@
 <?php
 
-foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__)) as $dir) {
-     echo $dir->getRealpath().PHP_EOL;
-     include $dir->getRealpath();
+foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__)) as $file) {
+     if ('php' !== $file->getExtension()) {
+         continue;    
+     }
+     echo $file->getRealpath().PHP_EOL;
+     include $file->getRealpath();
 }
