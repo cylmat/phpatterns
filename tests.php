ac708541482dea4b2711a2ae276bb088bc0597c4
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-12-14 20:57:13 +0100

    Update tests.php

--- tests.php
+++ tests.php
@@ -5,7 +5,7 @@ foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__)) 
          continue;    
      }
      echo $file->getRealpath();
-     if (true !== include_once $file->getRealpath()) { exit(1); }
+     if (true !== include_once $file->getRealpath()) { echo "File doesn't return true".PHP_EOL; exit(1); }
      else echo " ......... \t [OK] ";
      echo PHP_EOL;
 }
