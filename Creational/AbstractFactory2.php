60863e41b5450715f5f3a2bb158e94445750b4af
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-12-14 18:38:30 +0100

    Update AbstractFactory2.php

--- Creational/AbstractFactory2.php
+++ Creational/AbstractFactory2.php
@@ -2,112 +2,58 @@
 
 namespace Phpatterns\Creational;
 
-/*
-    cheese:goat,mozza,french
-    sauce:tomato,pepper,mayo
-    
-    pizza:cheese,pepperoni,veggie
-    
-    topAbstractFactory:sicil(mozza+tomato),gourmet(french+mayo)
-    sicilpizza(3),gourmetpizza(3)
-    
-    surface:smooth, rough, or bump
-    color:red,green,blue
-    polynome:triangle,square,round
-    
-    DocumentCreator 
-        createLetter
-        createResume
-    -> fancyDocumentCreator 
-        fancyLetter  (createLetter)
-        modernResume (createResume)
-*/
+interface IColor { public function __toString(); }
+interface IPolygon { public function getColor(): IColor; public function getType(): string; }
 
-interface IColor //red,green,blue
+class RedColor implements IColor { function __toString() { return 'red'; } }
+class GreenColor implements IColor { function __toString() { return 'green'; } }
+
+class Polygon implements IPolygon // object to be created
 {
-    public function getColor();
+    protected $color, $type;
+    public function __construct(IColor $color) { $this->color = $color; }
+    public function getColor(): IColor { return $this->color; }
+    public function getType(): string { return $this->type; }
 }
 
-class RedColor implements IColor { function getColor() { return 'red'; } }
-class OrangeColor implements IColor { function getColor() { return 'red+yellow'; } }
-class GreenColor implements IColor { function getColor() { return 'green'; } }
+class Triangle extends Polygon { protected $type = 'triangle'; }
+class Square extends Polygon { protected $type = 'square'; }
 
-interface ISurface //smooth, rough, or bump
+abstract class PolygonFactory // Factory pattern
 {
-    public function getSurface();
-}
-
-class SmoothSurface implements ISurface { function getSurface() { return 'smooth'; } }
-class RoughSurface implements ISurface { function getSurface() { return 'rough'; } }
-
-/**
- *  Polygon
- */
-interface IPolygon //triangle,square,round
-{
-    public function getSurface();
-    public function getColor();
-    public function getType();
-}
-
-abstract class Polygon implements IPolygon
-{
-    protected $color, $surface, $type;
-    public function __construct($color, $surface) { $this->color=$color; $this->surface=$surface; }
-    public function getSurface() { return $this->color; }
-    public function getColor() { return $this->surface; }
-    public function getType() { return $this->type; }
-    public function __toString() { return sprintf("A nice -%s- -%s- '%s'.\n",$this->color->getColor(), $this->surface->getSurface(), $this->type); }
-    public function __clone() { echo ' cloning... '; }
-}
-
-class Triangle extends Polygon { protected $type='triangle'; }
-class Square extends Polygon { protected $type='square'; }
-
-/**
- * Factory
- */
-abstract class PolygonFactory
-{
-    protected static $prototype=[];
-    public function create(IColor $color, ISurface $surface)
-    {
-        if (!array_key_exists(static::class, static::$prototype)) {
-            static::$prototype[static::class] = $this->createPrototype($color, $surface);
-            return static::$prototype[static::class];
-        }
-        return (static::$prototype[static::class]);
+    public function create(IColor $color) {
+        return $this->createPrototype($color);
     }
-    abstract protected function createPrototype(IColor $color, ISurface $surface): IPolygon;
+
+    abstract protected function createPrototype(IColor $color): IPolygon;
 }
 
-class TriangleFactory extends PolygonFactory { function createPrototype(IColor $color, ISurface $surface): IPolygon { return new Triangle($color, $surface); } }
-class SquareFactory extends PolygonFactory { function createPrototype(IColor $color, ISurface $surface): IPolygon { return new Square($color, $surface); } }
+class TriangleFactory extends PolygonFactory { 
+    protected function createPrototype(IColor $color): IPolygon { return new Triangle($color); } 
+}
+class SquareFactory extends PolygonFactory {
+    protected function createPrototype(IColor $color): IPolygon { return new Square($color); }
+}
 
-interface PolygonAbstractFactory 
-{
-    public function createTriangle();
+/**
+ * Abstract factory
+ */
+interface IPolygonAbstractFactory { 
+    public function createTriangle(); 
     public function createSquare();
 }
 
-class RedAbstractFactory implements PolygonAbstractFactory
+class RedAbstractFactory implements IPolygonAbstractFactory // AbstractFactory pattern
 {
-    protected $surface;
-    public function __construct(ISurface $surface) { $this->surface = $surface; }
-    
-    public function createTriangle(): IPolygon
-    {
-        return (new TriangleFactory)->create(new RedColor, $this->surface);
+    public function createTriangle(): IPolygon {
+        return (new TriangleFactory)->create(new RedColor);
     }
     
-    public function createSquare(): IPolygon
-    {
-        return (new SquareFactory)->create(new RedColor, $this->surface);
+    public function createSquare(): IPolygon {
+        return (new SquareFactory)->create(new RedColor);
     }
 }
 
-$red = new RedAbstractFactory(new RoughSurface);
-echo $red->createTriangle();
-echo $red->createSquare();
-echo $red->createSquare();
-echo $red->createSquare();
+$redTriangle = (new RedAbstractFactory())->createTriangle();
+return 'red triangle' === $redTriangle->getColor().' '.$redTriangle->getType();
+
