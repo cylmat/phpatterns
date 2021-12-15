2b31a623decc1b51ff35927acd14488285072f31
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-12-15 11:19:44 +0100

    Create Singleton.php

---
+++ Creational/Singleton.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Phpetterns\Creational;
+
+class Singleton
+{
+    private $params;
+    private static $singleInstance = null;
+    
+    private function __construct(string $params)
+    {
+        $this->params = $params;
+    }
+    
+    public static function getInstance(string $params): self
+    {
+        if (null === self::$singleInstance) {
+            self::$singleInstance = new self($params);
+        }
+        return self::$singleInstance;
+    }
+    
+    public function getParams(): string
+    {
+        return $this->params;
+    }
+}
+
+class UsingSingle
+{
+    private $single;
+    
+    public function __construct(Singleton $single)
+    {
+        $this->single = $single;
+    }
+    
+    public function getSingle(): Singleton
+    {
+        return $this->single;
+    }
+}
+
+$globalConfig = 'config_parameters';
+$single = Singleton::getInstance($globalConfig);
+
+$using = new UsingSingle($single);
+return 'config_parameters' === $using->getSingle()->getParams();
