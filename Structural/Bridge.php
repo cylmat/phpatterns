6bd26c91aab81e70ea1824f2793fedb04bde1d87
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-12-25 21:19:12 +0100

    Create Bridge.php

---
+++ Structural/Bridge.php
@@ -0,0 +1,7 @@
+<?php
+
+/*
+ * Make vary abstraction and implementation(s) independently
+ * Have common with Adapter (two independents classes works together)
+  *   when Bridge provides a stable interface to clients even as it lets you vary the classes that implement it
+ */
